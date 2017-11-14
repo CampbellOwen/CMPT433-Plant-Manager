2f158adcefa5bddb8ec0913bb9637b08dd7c87fb
Dolapo Toki <dolapotoki@gmail.com>
2017-11-14 15:49:21 -0800

    Wip: Sending ssid and password details from webpage esp-server

--- esp-webserver/esp-webserver.ino
+++ esp-webserver/esp-webserver.ino
@@ -16,7 +16,6 @@ void setup()
   Serial.println();
   network = new Networky(AP_SSID, AP_PASSWORD);
   server = new PageServer(8080, FILE_PATH);
-  // network->connectToSta(STA_SSID, STA_PASSWROD); // This works, make simple page to post the request from the webpage.
 }
 
 void loop()
