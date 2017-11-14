eb659f7ea74a2cbb9164c9f91a4a302d33525a72
Dolapo Toki <dolapotoki@gmail.com>
2017-11-14 13:13:31 -0800

    Added linux config

--- esp-webserver/scripts/generatebin.sh
+++ esp-webserver/scripts/generatebin.sh
@@ -1,4 +1,5 @@
 # Mac Os
-/Users/dolapotoki/Library/Arduino15/packages/esp8266/tools/mkspiffs/0.1.2/mkspiffs -p 256 -b 8192 -s $((0x3FB000 - 0x300000)) -c ~/dev/school/fall2017/cmpt433/final_project/Esp-webserver/data /Users/dolapotoki/dev/school/fall2017/cmpt433/final_project/esp-webserver/tmp/esp-data.bin
+# /Users/dolapotoki/Library/Arduino15/packages/esp8266/tools/mkspiffs/0.1.2/mkspiffs -p 256 -b 8192 -s $((0x3FB000 - 0x300000)) -c ~/dev/school/fall2017/cmpt433/final_project/Esp-webserver/data /Users/dolapotoki/dev/school/fall2017/cmpt433/final_project/esp-webserver/tmp/esp-data.bin
 # Gnu/Linux
+/home/dolapotoki/.arduino15/packages/esp8266/tools/mkspiffs/0.1.2/mkspiffs -p 256 -b 8192 -s $((0x3FB000 - 0x300000)) -c ~/dev/school/fall2017/cmpt433/final_project/esp-webserver/data ~/dev/school/fall2017/cmpt433/final_project/esp-webserver/tmp/esp-data.bin
 # Windows
