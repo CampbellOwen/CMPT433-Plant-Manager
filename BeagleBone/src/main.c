7c0824e4576c4dd792eff9db989ca238b318fdff
Owen Campbell <ocampbel@sfu.ca>
2017-12-02 02:04:25 -0800

    Refactored makefile

--- BeagleBone/src/main.c
+++ BeagleBone/src/main.c
@@ -2,12 +2,16 @@
 #include <include/device_manager.h>
 #include <include/udp_server.h>
 
+#ifndef PORT
+#define PORT 12345
+#endif
+
 int main( void )
 {
 
 	DeviceManager_Init();
 
-	UDP_Server_Init( 12345 );
+	UDP_Server_Init( PORT );
 
 	UDP_Server_Wait();
 
