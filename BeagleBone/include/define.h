d2672385476ef4e7cde50bde11d4b50af4c5c34b
Owen Campbell <ocampbel@sfu.ca>
2017-12-02 01:06:48 -0800

    Refactored and cleaned up all the logging

---
+++ BeagleBone/include/define.h
@@ -0,0 +1,10 @@
+#ifndef DEFINE_H_
+#define DEFINE_H_
+
+#define NETWORK       "[NETWORK]       "
+#define DEBUG         "[DEBUG]         "
+#define INFO          "[INFO]          "
+#define ERROR         "[ERROR]         "
+#define DEVICE_STATUS "[DEVICE STATUS] "
+
+#endif
